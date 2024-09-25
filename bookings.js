2550c08e6b6032ab90d4a89a1c5fcee99d59c534
David Poixblanc <lapichon@gmail.com>
2024-09-25 10:05:23 +0200

    maj booking

--- bookings.js
+++ bookings.js
@@ -2,33 +2,36 @@
 fetch('http://localhost:3000/bookings').
     then(response => response.json())
     .then(data => {
-        console.log(data.bookings.trips);
-        if (data) {
+        console.log(data);
+        if (data.bookings.length > 0) {
             document.querySelector('#bookingsList').innerHTML += `
                 <h1>My bookings</h1>
             `;
-            for (let i=0; i<data.bookings.trips.length; i++) {
+            for (let i=0; i<data.bookings.length; i++) {
                 
-                let date = new Date(data.bookings.trips[i].date);
+                let date = new Date(data.bookings[i].date);
                 let now = new Date();
                 let time = date.getTime()-now.getTime();
                 let remainingTime = "";
                 var days = parseInt(time/86400000);
                 var hours = parseInt((time%86400000)/3600000);
                 var minutes = parseInt((time%3600000)/60000);
-                if(days>0) remainingTime += days+" days ";
-                if(hours>0) remainingTime += hours+" hours ";
-                if(minutes>0) remainingTime += minutes+" minutes";
+                if(days>1) remainingTime += days+" days ";
+                else if (days>0) remainingTime += days+" day ";
+                if(hours>1) remainingTime += hours+" hours ";
+                else if(hours>0) remainingTime += hours+" hour ";
+                if(minutes>1) remainingTime += minutes+" minutes";
+                else if(minutes>0) remainingTime += minutes+" minute";
 
 
                 date = date.getHours()+":"+(date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
                 
                 document.querySelector('#bookingsList').innerHTML += `
                 <div class="tripContainer">
-                    <div>${data.bookings.trips[i].departure} > ${data.bookings.trips[i].arrival}</div>
-                    <div>${date}</div>
-                    <div>${data.bookings.trips[i].price} €</div>
-                    <div>Departure in ${remainingTime}</div>
+                    <div class="tripName">${data.bookings[i].departure} > ${data.bookings[i].arrival}</div>
+                    <div class="tripDate">${date}</div>
+                    <div class="tripPrice">${data.bookings[i].price} €</div>
+                    <div class="tripDeparture">Departure in ${remainingTime}</div>
                 </div>
                 `;
             }
